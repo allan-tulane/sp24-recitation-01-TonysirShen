4312ec1e0749d61de8682522fb72fd5b674338ab
TonysirShen <142557086+TonysirShen@users.noreply.github.com>
2024-01-29 22:09:22 +0000

    add more binary_search test,submission#2

--- test_main.py
+++ test_main.py
@@ -10,11 +10,12 @@ def test_linear_search():
 
 
 def test_binary_search():
-	assert binary_search([1,2,3,4,5], 5) == 4
-	assert binary_search([1,2,3,4,5], 1) == 0
-	assert binary_search([1,2,3,4,5], 6) == -1
+  assert binary_search([1,2,3,4,5], 5) == 4
+  assert binary_search([1,2,3,4,5], 1) == 0
+  assert binary_search([1,2,3,4,5], 6) == -1
+  assert binary_search(list(range(99)), 5) == 5
+  assert binary_search(list(range(99)), 0) == 0
 	### TODO: add two more tests here.
-
 	###
 
 
